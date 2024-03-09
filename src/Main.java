6d247277c72d9052661b7fcb7eb35a432597a7c1
Lobzter <88695455+Tomiloba21@users.noreply.github.com>
2024-03-09 21:49:55 +0100

    Update Main.java- added comments

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
+        // main class, calls the calculatorUI class 
+        //It initializes and displays the user interface for the BMI calculator by creating an instance of the CalculatorUI class.
         new CalculatorUI();
 
     }
